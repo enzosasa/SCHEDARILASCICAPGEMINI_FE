f5205cab813e79a0504d982f9c298e2337375510
emanuele <emanuele@192.168.1.8>
2017-11-23 18:20:51 +0100

    Aggiornamenti vari

--- workspace2/SchedaRilasciFE/src/main/webapp/javascript/csvController.js
+++ workspace2/SchedaRilasciFE/src/main/webapp/javascript/csvController.js
@@ -1,10 +1,12 @@
 angular.module('csv', [])
 .controller('controllerCsv', function($scope, $http) {	
-    $http.get('http://localhost:8080/SchedaRilasci/csvForIdPolarion?idPolarion=AGORA-6564').
-        then(function(response) {
-            $scope.csv= response.data;
-           
-            });
+	$http.get('http://localhost:8080/SchedaRilasci/csvForIdPolarion?idPolarion=AGORA-6564').
+	then(function(response) {
+		$scope.csv= response.data;
+		$scope.dataEstrazione= csv[0].fileDate;
+		
+		
+	});
 });
 
 
